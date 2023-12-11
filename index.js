254d8b1ffed7a0e5b9f2ca9109cf228fc8f5d188
csimancas <csimancas@csimancass-MacBook-Pro.local>
2023-12-11 14:28:11 -0700

    delete comments

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const { dbConnection } = require("./database/config");
 
-// Crear el servidor de express
 const app = express();
 
-// Base de datos
 dbConnection();
 
-// Direcrorio publico
 app.use(express.static("public"));
 
-// Lectura y parseo del body
 app.use(express.json());
 
 app.use("/api/entries", require("./routes/entries"));
